719a33249bd2b76c460a62c929a2f7c2ac1ccfa0
Ubirajara Neves <biraneves@outlook.com>
2024-01-05 22:07:43 +0100

    Ajustes no script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
-const form = document.getElementsByTagName("form");
+const form = document.getElementsByTagName("form")[0];
 
 function validateForm(numberA, numberB) {
-    return numberA < numberB;
+    return parseInt(numberA) < parseInt(numberB);
 }
 
 form.addEventListener("submit", (e) => {
     const numberA = document.getElementById("campoA");
     const numberB = document.getElementById("campoB");
-    const message = document.getElementsByClassName("message");
+    const message = document.getElementsByClassName("message")[0];
 
-    if (validateForm(numberA.value, numberB.value))
+    e.preventDefault();
+
+    if (validateForm(numberA.value, numberB.value)){
+        message.classList.remove("error");
         message.classList.add("success");
-    else
+        message.innerHTML = `Muito bem: ${numberA.value} < ${numberB.value}!`;
+    }
+    else {
+        message.classList.remove("success");
         message.classList.add("error");
+        message.innerHTML = `Opa: ${numberA.value} >= ${numberB.value}!`;
+    }
 });
+
